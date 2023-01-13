fd52a6870b94f169872be75bd9d8c8378bd7bdaf
Alanmpd89 <Alanmpd89@gmail.com>
2023-01-12 21:24:53 -0500

    Withdraw created

---
+++ src/Withdraw.java
@@ -0,0 +1,12 @@
+public class Withdraw {
+
+    public static void main(String[] args) {
+
+        System.out.println("How much you want to withdraw");
+
+        System.out.println("Money in slot");
+
+
+    }
+
+}
